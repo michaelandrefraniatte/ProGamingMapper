c93a366a8b586596cc0962c0989a030330731a0e
Michaël André Franiatte <113953032+michaelandrefraniatte@users.noreply.github.com>
2023-11-16 11:21:32 +0100

    Add a link

---
+++ Src/LocalAuthenticationCreator/LocalAuthenticationCreator/Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Security.Cryptography;
+using System.IO;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using System.Management;
+
+namespace LocalAuthenticationCreator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private static string username, hash, userchecksum, uniqueid, userchecksumuniqueid;
+        const string alphanumeric = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890";
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string nickname = Microsoft.VisualBasic.Interaction.InputBox("Prompt", "Enter your nickname", "michael113b", 0, 0);
+            string programname = Microsoft.VisualBasic.Interaction.InputBox("Prompt", "Enter the program name", "PGM", 0, 0);
+            programname = programname + ".exe";
+            username = nickname;
+            string exePath = programname;
+            SHA1 sha1 = SHA1.Create();
+            FileStream fs = new FileStream(exePath, FileMode.Open, FileAccess.Read);
+            string checksum = BitConverter.ToString(sha1.ComputeHash(fs)).Replace("-", "");
+            fs.Close();
+            uniqueid = getUniqueId();
+            userchecksumuniqueid = username + checksum + uniqueid;
+            string salt = GetSalt(10); // 10 is the size of Salt 
+            string hashedPass = HashPassword(salt, userchecksumuniqueid);
+            using (StreamWriter file = new StreamWriter("la.txt"))
+            {
+                file.WriteLine(nickname);
+                file.WriteLine(hashedPass);
+            }
+        }
+        public static string getUniqueId()
+        {
+            try
+            {
+                string cpuInfo = string.Empty;
+                ManagementClass mc = new ManagementClass("win32_processor");
+                ManagementObjectCollection moc = mc.GetInstances();
+                foreach (ManagementObject mo in moc)
+                {
+                    cpuInfo = mo.Properties["processorID"].Value.ToString();
+                    break;
+                }
+                string drive = "C";
+                ManagementObject dsk = new ManagementObject(@"win32_logicaldisk.deviceid=""" + drive + @":""");
+                dsk.Get();
+                string volumeSerial = dsk["VolumeSerialNumber"].ToString();
+                string uuidInfo = string.Empty;
+                ManagementClass mcu = new ManagementClass("Win32_ComputerSystemProduct");
+                ManagementObjectCollection mocu = mcu.GetInstances();
+                foreach (ManagementObject mou in mocu)
+                {
+                    uuidInfo = mou.Properties["UUID"].Value.ToString();
+                    break;
+                }
+                if (volumeSerial != null & volumeSerial != "" & cpuInfo != null & cpuInfo != "" & uuidInfo != null & uuidInfo != "")
+                    return volumeSerial + "-" + cpuInfo + "-" + uuidInfo;
+                else
+                    return null;
+            }
+            catch
+            {
+                Application.Exit();
+                return null;
+            }
+        }
+        public static string GetSalt(int saltSize)
+        {
+            float key = 0.6f;
+            StringBuilder strB = new StringBuilder("");
+            while ((saltSize--) > 0)
+                strB.Append(alphanumeric[(int)(key * alphanumeric.Length)]);
+            return strB.ToString();
+        }
+        public static string HashPassword(string salt, string password)
+        {
+            string mergedPass = string.Concat(salt, password);
+            return EncryptUsingMD5(mergedPass);
+        }
+        public static string EncryptUsingMD5(string inputStr)
+        {
+            using (MD5 md5Hash = MD5.Create())
+            {
+                // Convert the input string to a byte array and compute the hash.
+                byte[] data = md5Hash.ComputeHash(Encoding.UTF8.GetBytes(inputStr));
+
+                // Create a new Stringbuilder to collect the bytes
+                // and create a string.
+                StringBuilder sBuilder = new StringBuilder();
+
+                // Loop through each byte of the hashed data 
+                // and format each one as a hexadecimal string.
+                for (int i = 0; i < data.Length; i++)
+                    sBuilder.Append(data[i].ToString("x2"));
+
+                // Return the hexadecimal string.
+                return sBuilder.ToString();
+            }
+        }
+    }
+}
